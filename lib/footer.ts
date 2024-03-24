de7f265e56be891d1a0202840f7b9db3ec3d3a9c
Koen van Esterik <koen@vanesterik.com>
2024-03-24 15:40:12 +0100

    fix: remove grid styling from footer util

--- lib/footer.ts
+++ lib/footer.ts
@@ -1,10 +1,3 @@
 import { cva } from 'class-variance-authority'
 
-export const footer = cva([
-  'grid',
-  'grid-cols-[3.5rem_auto]',
-  'gap-8',
-  'pb-3',
-  'pt-12',
-  'z-10',
-])
+export const footer = cva(['pb-3', 'pt-12', 'z-10'])
